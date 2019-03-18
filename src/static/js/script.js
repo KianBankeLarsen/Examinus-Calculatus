8eaeff7d8a23f42e301d35be856873140fd92579
Kian Banke Larsen <snubikian@gmail.com>
2019-03-18 11:59:47 +0100

    added support for buttonfunc functionality

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,3 +1,8 @@
+var listBrand = ['sin()', 'cos()', 'tan()', 'arcsin()', 'arccos()', 'arctan()', 'sinh()', 'cosh()', 'tanh()', 'parse()',
+    'LEXUS', 'AUDI', 'MAYBACK', 'FERRARI', 'TOYOTA', 'LEXUS', 'AUDI', 'MAYBACK', 'FERRARI', 'TOYOTA', 'LEXUS', 'AUDI',
+    'MAYBACK', 'FERRARI', 'TOYOTA'
+];
+//the array
 $(document).ready(function() {
     var list = [];
     var n = 0;
@@ -35,9 +40,9 @@ $(document).ready(function() {
                 container.append(highlight("= " + data));
                 container.append("<br>" + "<br>");
             } else if (header.getResponseHeader('Content-Type') === 'image/jpeg') {
-                console.log("YES!")
+                console.log("YES!");
                 var img = new Image();
-                img.src = "data:image/jpeg;base64,"+ data;
+                img.src = "data:image/jpeg;base64," + data;
                 container.append("<br>" + "<br>");
                 container.append(img);
                 container.append("<br>" + "<br>");
@@ -86,19 +91,34 @@ $(document).ready(function() {
         $("#container").stop().animate({
             scrollTop: $("#container")[0].scrollHeight
         }, 0);
+        inputbox.focus();
     });
 
-    $('#tan').click(function() {
-        insertAtCursor(inputbox[0], "tan()", cursorpos);
-    });
+    for (var i = 0; i < listBrand.length; i++) {
+        var btn = document.createElement("button");
+        var t = document.createTextNode(listBrand[i]);
+        btn.classList.add("buttonfunc");
+        const paste_name = i;
+        $(btn).click(function() {
+            cursorpos = inputbox[0].selectionStart;
+            insertAtCursor(inputbox[0], listBrand[paste_name], cursorpos);
+            var new_cursorpos = listBrand[paste_name].length - 1;
+            console.log(paste_name);
+            inputbox[0].selectionStart = inputbox[0].selectionEnd = cursorpos + new_cursorpos;
+            inputbox.focus();
+        });
+        btn.appendChild(t);
+        $('#sidebar').append(btn);
+    }
 
     inputbox.click(async function(e) {
-         cursorpos = inputbox[0].selectionStart;
+        cursorpos = inputbox[0].selectionStart;
     });
 
     inputbox.keyup(async function(e) {
-         cursorpos = inputbox[0].selectionStart;
+        cursorpos = inputbox[0].selectionStart;
     });
+
     function insertAtCursor(myField, myValue, cursorpos) {
         //MOZILLA and others
         if (myField.selectionStart || myField.selectionStart == '0') {
